148d8ce7f54bcba5fbf44055b7cab4ab01d9c1bf
Christopher Casso <ccasso@wind.com.do>
2017-10-04 17:10:40 -0400

    Initial Commit

--- Vidly.Web/App_Start/RouteConfig.cs
+++ Vidly.Web/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace Vidly.Web
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "MoviesByReleaseDate",
+                url: "movies/released/{year}",
+                defaults: new { controller = "Movies", action = "ByReleaseDate" }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
---
+++ Vidly.Web/Controllers/MoviesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Vidly.Web.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            return View();
+        }
+
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year +" "+ month);
+        }
+    }
+}
