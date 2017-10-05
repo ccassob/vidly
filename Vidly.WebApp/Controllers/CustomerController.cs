088311084b60ceae3973566c37538fd9fbef50e4
Christopher Casso <ccasso@wind.com.do>
2017-10-05 16:19:59 -0400

    Terminado modulo 3 de migrations

---
+++ Vidly.WebApp/Controllers/CustomerController.cs
@@ -0,0 +1,107 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
+using Vidly.WebApp.Models;
+
+namespace Vidly.WebApp.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CustomerController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        // GET: Customer
+        public ActionResult Index()
+        {
+            var customer = _context.Customers.Include(c => c.MemberShipType).ToList();
+
+            return View(customer);
+        }
+
+        // GET: Customer/Details/5
+        public ActionResult Details(int id)
+        {
+
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            return View(customer);
+        }
+
+        // GET: Customer/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Customer/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Customer/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            return View(customer);
+        }
+
+        // POST: Customer/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Customer/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            return View(customer);
+        }
+
+        // POST: Customer/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- Vidly.WebApp/Models/Customer.cs
+++ Vidly.WebApp/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,12 @@ namespace Vidly.WebApp.Models
     public class Customer
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
+        public MemberShipType MemberShipType{ get; set; }
+        public byte MemberShipTypeId { get; set; }
     }
+
 }
